6c9e64799fc9598d9312fd0f81f49e54eded94ae
Mihsta <github@stami.org>
2021-10-15 12:05:26 +0300

    bash fix

--- task03.2/play.sh
+++ task03.2/play.sh
@@ -1,7 +1,7 @@
 git pull
 ansible-playbook pb_vsftpd.yml -v $1 $2 > ansible.log
-NOW=`%F_%H:%M:%S`
-echo 'Upload file ansible no node1 log with datetime: '+$NOW
+NOW=date +"%m/%d/%Y %H:%M:%S $HOSTNAME"
+echo 'Upload file ansible no node1 log with datetime: $NOW'
 curl -T ansible.log ftp://node1/upload/ansible_$NOW.log --user anonymous:password
 echo 'List files uploaded to node1'
 curl -s -u anonymous:password ftp://node1/upload/
