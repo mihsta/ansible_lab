d8bfd62428677f9b3ab90c2f0b76bcb8c8959e2c
Mihsta <github@stami.org>
2021-10-15 10:39:38 +0300

    fix play

--- task03.2/play.sh
+++ task03.2/play.sh
@@ -3,5 +3,5 @@ ansible-playbook pb_vsftpd.yml -v $1 $2
 
 NOW=`'%F_%H:%M:%S'`
 
-curl -T ansible.cfg ftp://node1/pub/upload/ansible_$NOW.cfg --user anonymous:password
-curl -T ansible.cfg ftp://node2/pub/upload/ansible_$NOW.cfg --user anonymous:password
+curl -T /home/ansible/ansible_lab/task03.2/ansible.cfg ftp://node1/pub/upload/ansible_$NOW.cfg --user anonymous:password
+curl -T /home/ansible/ansible_lab/task03.2/ansible.cfg ftp://node2/pub/upload/ansible_$NOW.cfg --user anonymous:password
