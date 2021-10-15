7b7dd14af8597e758e750f377df593b88bc18eb2
Mihsta <github@stami.org>
2021-10-15 08:34:06 +0300

    fix test path

--- task03.2/play.sh
+++ task03.2/play.sh
@@ -1,4 +1,4 @@
 git pull
 ansible-playbook pb_vsftpd.yml -v $1 $2
-curl -T ansible.cfg ftp://node1.example.com/upload/ansible.cfg --user anonymous:password
-curl -T ansible.cfg ftp://node2.example.com/upload/ansible.cfg --user anonymous:password
+curl -T ansible.cfg ftp://node1.example.com/pub/upload/ansible.cfg --user anonymous:password
+curl -T ansible.cfg ftp://node2.example.com/pub/upload/ansible.cfg --user anonymous:password
