76b8c5be55aa14a2c7b6e3368ed510d591fafb0c
Mihsta <github@stami.org>
2021-10-15 12:09:26 +0300

    bash fix

--- task03.2/play.sh
+++ task03.2/play.sh
@@ -1,11 +1,10 @@
 git pull
 ansible-playbook pb_vsftpd.yml -v $1 $2 > ansible.log
-NOW=date +"%m/%d/%Y %H:%M:%S $HOSTNAME"
-echo 'Upload file ansible no node1 log with datetime: $NOW'
-curl -T ansible.log ftp://node1/upload/ansible_$NOW.log --user anonymous:password
+echo 'Upload file ansible no node1 log with datetime:' $RANDOM
+curl -T ansible.log ftp://node1/upload/ansible_$RANDOM.log --user anonymous:password
 echo 'List files uploaded to node1'
 curl -s -u anonymous:password ftp://node1/upload/
-echo 'Upload file ansible to node2 log with datetime: '+$NOW
-curl -T ansible.log ftp://node2/upload/ansible_$NOW.log --user anonymous:password
+echo 'Upload file ansible to node2 log with datetime: ' $RANDOM
+curl -T ansible.log ftp://node2/upload/ansible_$RANDOM.log --user anonymous:password
 echo 'List files uploaded to node2'
 curl -s -u anonymous:password ftp://node2/upload/
