1ad92a37100ce62c85045531cb99295f3dacb534
Mihsta <github@stami.org>
2021-10-15 11:26:06 +0300

    bash fix

--- task03.2/play.sh
+++ task03.2/play.sh
@@ -5,7 +5,7 @@ echo 'Upload file ansible no node1 log with datetime: '+$NOW
 curl -T ansible.log ftp://node1/upload/ansible_$NOW.log --user anonymous:password
 echo 'List files uploaded to node1'
 curl -s -u anonymous:password ftp://node1/upload/
-echo 'Upload file ansible to node2 log with datetime: '+$NO
+echo 'Upload file ansible to node2 log with datetime: '+$NOW
 curl -T ansible.log ftp://node2/upload/ansible_$NOW.log --user anonymous:password
 echo 'List files uploaded to node2'
 curl -s -u anonymous:password ftp://node2/upload/
