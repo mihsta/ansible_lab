5978ef230f478799fda3a418ef125fae226b7af2
Mihsta <github@stami.org>
2021-10-15 11:19:10 +0300

    fix bash

--- task03.2/play.sh
+++ task03.2/play.sh
@@ -3,5 +3,5 @@ ansible-playbook pb_vsftpd.yml -v $1 $2 > ansible.log
 
 NOW=`'%F_%H:%M:%S'`
 
-curl -T ansible.log ftp://node1/pub/upload/ansible_$NOW.log --user anonymous:password
-curl -T ansible.log ftp://node2/pub/upload/ansible_$NOW.log --user anonymous:password
+curl -T ansible.log ftp://node1/upload/ansible_$NOW.log --user anonymous:password
+curl -T ansible.log ftp://node2/upload/ansible_$NOW.log --user anonymous:password
