61e4db18077b52ed2c5a6922aa4581eeba900cd3
mihsta <me@stami.org>
2021-10-15 13:14:53 +0300

    bash fix

--- task03.2/play.sh
+++ task03.2/play.sh
@@ -1,6 +1,6 @@
 git pull
 ansible-playbook pb_vsftpd.yml -v $1 $2 > ansible.log
-echo 'Upload file ansible no node1
+echo 'Upload file ansible no node1'
 curl -T ansible.log ftp://node1/upload/ansible_$RANDOM.log --user anonymous:password
 echo 'List files uploaded to node1'
 curl -s -u anonymous:password ftp://node1/upload/
